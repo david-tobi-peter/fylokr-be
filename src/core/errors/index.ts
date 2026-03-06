3b845613775406a1f85f388b3d8b0b1014274894
DavidHODs <davidoluwatobi41@gmail.com>
2026-03-06 10:32:03 +0100

    standarized error handling

--- src/core/errors/index.ts
+++ src/core/errors/index.ts
@@ -1,2 +1,5 @@
-export * from "./app.error.js";
+export * from "./base.error.js";
+export * from "./auth.error.js";
+export * from "./http.error.js";
+export * from "./system.error.js";
 export * from "./handler.error.js";
---
+++ src/core/errors/system.error.ts
@@ -0,0 +1,19 @@
+import { ERROR_TYPE_ENUM } from "#/shared/enums";
+import { BaseError } from "./base.error.js";
+
+export class DatabaseError extends BaseError {
+  readonly type = ERROR_TYPE_ENUM.DATABASE_ERROR;
+  readonly statusCode = 500;
+  protected override readonly isSensitive = true;
+}
+
+export class InternalServerError extends BaseError {
+  readonly type = ERROR_TYPE_ENUM.INTERNAL_SERVER_ERROR;
+  readonly statusCode = 500;
+  protected override readonly isSensitive = true;
+}
+
+export class ConfigurationError extends BaseError {
+  readonly type = ERROR_TYPE_ENUM.CONFIGURATION_ERROR;
+  readonly statusCode = 500;
+}
